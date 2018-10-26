76d2eeffc5deab482d4407e1bd58c24ff60482e2
zszhou <973945379@qq.com>
2018-10-26 11:11:18 +0800

    add attack

--- attacks.py
+++ attacks.py
@@ -4,7 +4,7 @@ from scipy.stats import truncnorm
 
 import torch
 import torch.nn as nn
-
+import torchvision.transforms.functional as ttf
 
 # --- White-box attacks ---
 
@@ -48,3 +48,91 @@ class LinfPGDAttack(object):
             x = torch.clamp(x, 0, 1)
 
         return x
+    
+    
+class TransferAttack(object):
+    
+    def __init__(self, ori_model=None, models=None, epsilon=16/255, k=10, a=0.01, 
+        random_start=True):
+        
+        self.ori_model = ori_model
+        self.models = models
+        
+        self.epsilon = epsilon
+        self.k = k
+        self.random_start = random_start
+        self.step_size = a
+        
+        self.loss_fn = nn.CrossEntropyLoss()
+        
+      
+    def attack_batch(self, batch_tensor, y, target_attack=True):
+        """inputs are pytorch tensor format
+        if target is None, we use the untarget attack method.
+        """
+        
+        x = batch_tensor.detach()
+        
+            
+        if self.random_start:
+            x = x + torch.zeros_like(x).uniform_(-self.epsilon, self.epsilon)
+                
+        for i in range(self.k):             
+            #get grad            
+            x.requires_grad_()
+            if self.models is None:
+                with torch.enable_grad():
+                    logits = self.ori_model(x)
+                    loss = self.loss_fn(logits, y)
+                grad = torch.autograd.grad(loss, [x])[0]
+                final_grad = grad.detach()              
+            else:
+                grads = []
+                for j in range(len(self.models)):
+                    with torch.enable_grad():
+                        logits = self.models[j](x)
+                        loss = self.loss_fn(logits, y)
+                    grad = torch.autograd.grad(loss, [x])[0].detach()
+                    grads.append(grad)
+                final_grad = torch.zeros_like(batch_tensor)  
+                for g in grads:
+                    final_grad += g
+                final_grad /= len(self.models)        
+                
+            if not target_attack:    
+                x = x.detach() + self.step_size*torch.sign(final_grad)
+            else:
+                x = x.detach() - self.step_size*torch.sign(final_grad)
+           
+            x = torch.min(torch.max(x, batch_tensor - self.epsilon), batch_tensor + self.epsilon)
+            x = torch.clamp(x, 0, 1)                    
+
+        return x
+    
+    
+    def attack_tensor(self, tensor, y, target_attack=True):
+        
+        batch_tensor = tensor.unsqueeze(0)
+        
+        #y = y.unsqueeze(0)
+            
+        x = self.attack_batch(batch_tensor, y,target_attack=target_attack)
+        x = x.squeeze(0)
+        
+        return x
+        
+        
+    def attack_cv2(self, cv2_image, y,device, target_attack=True):
+        
+        tensor = ttf.to_tensor(cv2_image)
+        y = torch.LongTensor([y])        
+        
+        tensor = tensor.to(device)
+        y = y.to(device)
+        
+        x = self.attack_tensor(tensor, y,target_attack=target_attack)
+        
+        pic = x.mul(255).byte()
+        npimg = np.transpose(pic.cpu().numpy(), (1, 2, 0))
+        
+        return npimg    
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-
+import torch
+from model import WrapperModel
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -16,3 +17,24 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = round(self.sum / self.count, 5)
+        
+def load_model(model_name, model_path):
+    model = WrapperModel(model_name)
+    model.load_state_dict(torch.load(model_path))
+    return model
+
+def load_models(device):
+    root = 'models/'
+    resnet18_adv = load_model('resnet18',root+'resnet18_lr_0.01_adv_16_10_10.pth')
+    resnet18 = load_model('resnet18',root+'resnet18_lr_0.01.pth')
+
+    resnet50_adv = load_model('resnet50',root+'resnet50_lr_0.01_adv_16_20_10.pth')
+    resnet50 = load_model('resnet50', root+'resnet50_lr_0.01.pth')
+    
+
+    resnet18 = resnet18.to(device)
+    resnet18_adv = resnet18_adv.to(device)
+    resnet50 = resnet50.to(device)
+    resnet50_adv = resnet50_adv.to(device) 
+    
+    return [resnet18, resnet18_adv, resnet50, resnet50_adv]
